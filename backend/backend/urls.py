e58a21cadf8ec0eacf668dfa8238add3d48682f1
Diana Daraban <di.daraban@gmail.com>
2026-05-20 13:59:42 +0300

    Serve media explicitly

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import TokenRefreshView
 from accounts.views import EmailOrUsernameTokenObtainPairView
 from django.conf import settings
-from django.conf.urls.static import static
-
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,7 +13,10 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('api/accounts/', include('accounts.urls')),
     path('api/notifications/', include('notifications.urls')),
-]
 
-urlpatterns += static(settings.MEDIA_URL,
-                      document_root=settings.MEDIA_ROOT, insecure=True)
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT}
+    ),
+]
