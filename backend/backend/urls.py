16978fc59987042f4bbcf404028708daf920211e
Diana Daraban <di.daraban@gmail.com>
2026-05-20 13:53:36 +0300

    Serve media files on Render

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     path('api/notifications/', include('notifications.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT, insecure=True)
